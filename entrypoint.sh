edfc627460cb953aaad1ec46dfa7d353eae9d64b
RaphaelFred <raphaelDC@gmx.de>
2022-03-02 08:24:31 +0100

    Edited infra files

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,11 @@
-#!/bin/sh
+#!/bin/bash
 
-python manage.py migrate --no-input
-python manage.py collectstatic --no-input
+# Collect static files
+echo "Collect static files"
+python manage.py collectstatic --noinput
 
-gunicorn football_analysis_app.wsgi:application --bind 0.0.0.0:8000
+# Apply database migrations
+echo "Apply database migrations"
+python manage.py migrate
+
+gunicorn football_analysis_app.wsgi:application --bin 0.0.0.0:8000
