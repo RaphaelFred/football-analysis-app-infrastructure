e29ed0f3e34686fef694f5216e3bfa1311e8f53e
Raphael Fredebeul <raphael@MacBook-Pro-von-Raphael.local>
2023-04-24 14:59:09 +0200

    Added ssl cert gen script

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -5,7 +5,7 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-domains=(footballytica.de www.footballytica.de)
+domains=(footballytica.de)
 rsa_key_size=4096
 data_path="./data/certbot"
 email="" # Adding a valid address is strongly recommended
